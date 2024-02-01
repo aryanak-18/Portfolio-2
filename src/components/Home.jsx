cf272cc4ee94e3dc1f02643b3793d1384324086b
Aryan Kumar <aryan.ak.work@gmail.com>
2024-02-01 16:16:00 +0530

    updated with profile pic

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { HiArrowNarrowRight } from "react-icons/hi";
 import { Link } from "react-scroll";
+import DP from "../assets/DP-2.jpg";
 
 const Home = () => {
   return (
     <div name="home" className="w-full h-screen bg-[#0a192f]">
       {/* Container */}
       <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
-        <p className="text-[#3bffff]">Hi, my name is</p>
+        <img className="rounded-full w-52 mx-auto max-sm:w-28 max-md:w-40" src={DP} alt="profile-pic"/>
+        <p className="text-[#3bffff] mt-8">Hi, my name is</p>
         <h1 className="text-4xl sm:text-7xl font-bold text-white">
           Aryan Kumar
         </h1>
