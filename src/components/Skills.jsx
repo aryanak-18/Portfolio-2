188ed303e4dc9e9b069ec0c7f982f130d3cea271
Aryan Kumar <aryan.ak.work@gmail.com>
2024-06-16 17:35:59 +0530

    Adjust CSS

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -15,7 +15,7 @@ import Cplusplus from "../assets/cplusplus.png";
 
 const Skills = () => {
   return (
-    <div name="skills" className="w-full h-screen bg-[#0a192f] text-white pb-40">
+    <div name="skills" className="w-full h-full bg-[#0a192f] text-white">
       {/* Container */}
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
         <div>
